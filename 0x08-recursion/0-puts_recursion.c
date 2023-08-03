73fc7542bbb1631ed63db4e8615fa157d820339e
Tynoe19 <Tinokondo33@gmail.com>
2023-08-03 10:55:10 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,11 @@
-include "main.h"
+#include "main.h"
+
 /**
  * _puts_recursion - function like puts();
  * @s: input
  * Return: Always 0 (Success)
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
@@ -15,3 +17,4 @@ void _puts_recursion(char *s)
 	else
 		_putchar('\n');
 }
+
